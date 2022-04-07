8f20b5ebf2e164c1c588fdbd23b0845a74d470c3
JulianIrigoyen <martinirigoyenj17@gmail.com>
2022-04-07 14:59:44 +0200

    First commit - setting up pkg

---
+++ idf-to-ifc/__init__.py
@@ -0,0 +1,4 @@
+
+# This file is required. It tells python that idf-to-ifc is a Python package folder.
+# You can keep it empty.
+# Optionally you can include import statements here to make importing code from your package easier.
